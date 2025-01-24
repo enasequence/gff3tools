51e4277a28eafd113a07676a0df2eab4fa6ed900
Awais Athar <awais@ebi.ac.uk>
2025-01-24 21:06:27 +0000

    Handle joins Remove /db_xrefs

--- src/main/java/uk/ac/ebi/embl/converter/GFF3ToFFConverter.java
+++ src/main/java/uk/ac/ebi/embl/converter/GFF3ToFFConverter.java
@@ -2,18 +2,20 @@ package uk.ac.ebi.embl.converter;
 
 import uk.ac.ebi.embl.api.entry.Entry;
 import uk.ac.ebi.embl.api.entry.EntryFactory;
+import uk.ac.ebi.embl.api.entry.Text;
 import uk.ac.ebi.embl.api.entry.feature.Feature;
 import uk.ac.ebi.embl.api.entry.feature.FeatureFactory;
-import uk.ac.ebi.embl.api.entry.location.Location;
-import uk.ac.ebi.embl.api.entry.location.LocationFactory;
-import uk.ac.ebi.embl.api.entry.location.Order;
+import uk.ac.ebi.embl.api.entry.location.*;
 import uk.ac.ebi.embl.api.entry.qualifier.Qualifier;
 import uk.ac.ebi.embl.api.entry.qualifier.QualifierFactory;
+import uk.ac.ebi.embl.api.entry.sequence.Sequence;
 import uk.ac.ebi.embl.api.gff3.GFF3Record;
 import uk.ac.ebi.embl.api.gff3.GFF3RecordSet;
 import uk.ac.ebi.embl.api.validation.ValidationResult;
 import uk.ac.ebi.embl.converter.utils.ConversionEntry;
 import uk.ac.ebi.embl.converter.utils.ConversionUtils;
+import uk.ac.ebi.embl.flatfile.reader.EntryReader;
+import uk.ac.ebi.embl.flatfile.reader.embl.EmblEntryReader;
 import uk.ac.ebi.embl.flatfile.writer.embl.EmblEntryWriter;
 import uk.ac.ebi.embl.gff3.reader.GFF3FlatFileEntryReader;
 
@@ -23,9 +25,7 @@ import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class GFF3ToFFConverter {
@@ -36,6 +36,8 @@ public class GFF3ToFFConverter {
     private final LocationFactory locationFactory = new LocationFactory();
     private final QualifierFactory qualifierFactory = new QualifierFactory();
     private final Map<String,String> idToParentGeneMap = new HashMap<>();
+    private final Map<String, List<Feature>> idToChildrenMap = new HashMap<>();
+    private final Map<String, Feature> idToFeatureMap = new HashMap<>();
 
     public GFF3ToFFConverter() throws Exception {
     }
@@ -53,7 +55,11 @@ public class GFF3ToFFConverter {
             //new EmblEntryWriter(new GFF3Mapper().mapGFF3ToEntry(recordSet).get(0)).write(ffWriter); // existing incorrect implementation
             new EmblEntryWriter(entry).write(ffWriter);
             ffWriter.close();
-            Files.write(Paths.get("delete-this.embl"), ffWriter.toString().getBytes());
+            Path path = Paths.get("delete-this.embl");
+            Files.write(path, ffWriter.toString().getBytes());
+            ValidationResult validationResultAfterConversion = new EmblEntryReader( Files.newBufferedReader(path) ).read();
+            System.out.println(validationResultAfterConversion);
+
         }
 
     }
@@ -81,6 +87,23 @@ public class GFF3ToFFConverter {
                     return feature;
                 })
                 .forEach(entry::addFeature);
+
+        // update locations
+        // Rule: The location of parent will be the join of locations of all children, complemented if strand is "-"
+        idToChildrenMap.forEach((id, features) -> {
+            CompoundLocation<Location> compoundLocation = new Join<>();
+            features.forEach(feature -> feature
+                    .getLocations()
+                    .getLocations()
+                    .forEach(compoundLocation::addLocation)
+            );
+            Feature feature = idToFeatureMap.get(id);
+            if (feature.getLocations().isComplement())
+                compoundLocation.setComplement(true);
+
+            feature.setLocations(compoundLocation);
+
+        });
         return entry;
     }
 
@@ -92,14 +115,20 @@ public class GFF3ToFFConverter {
         // Rule: If more than one feature is found to match, always use the first one
         Feature feature = featureFactory.createFeature(featureMap.get(record.getType()).getSOTerm());
 
-        // TODO: add joins -- needs to be refactored
+        // Add location - ignoring strand in location because it is handled at the parent level
         Location location = locationFactory.createLocalRange((long) record.getStart(), (long) record.getEnd());
-        Order<Location> compoundJoin = new Order<Location>();
+        Order<Location> compoundJoin = new Order<>();
         compoundJoin.addLocation(location);
         feature.setLocations(compoundJoin);
 
+        // Add strand
+        if (record.getStrand()<0) feature.getLocations().setComplement(true);
+
+
         Map<String, String> attributes = record.getAttributes();
-        String parent = attributes.remove("Parent"); // remove and save parent
+        String parent = attributes.remove("Parent"); // remove parent
+        String ID = attributes.remove("ID"); // remove ID
+
 
         // Build qualifier list
         List<Qualifier> qualifiers = attributes.entrySet().stream()
@@ -107,23 +136,32 @@ public class GFF3ToFFConverter {
                 .collect(Collectors.toList());
 
         // Use parent to trace the /gene qualifier if needed
+        // Assumption: Only one parent is allowed per feature (as per the INSDC draft)
         String parentGene = null;
         if (parent!=null) {
             parentGene = idToParentGeneMap.get(parent);
             if (parentGene!=null) {
                 qualifiers.add(qualifierFactory.createQualifier("gene", parentGene));
-                if (attributes.get("ID") != null) {
-                    idToParentGeneMap.put(attributes.get("ID"), parentGene);
+                if (ID != null) {
+                    idToParentGeneMap.put(ID, parentGene);
                 }
             }
-        }
-        // TODO: Check if we need to restrict this to a subset of qualifiers
-        if (attributes.get("ID")!=null && attributes.get("gene")!=null) {
-            idToParentGeneMap.put(attributes.get("ID"),attributes.get("gene"));
         }
 
         // Add qualifiers
         feature.addQualifiers(qualifiers);
+
+        // Update maps
+        if (ID!=null) {
+            idToFeatureMap.put(ID, feature);
+            // TODO: Check if we need to restrict this to a subset of qualifiers
+            if (attributes.get("gene")!=null)
+                idToParentGeneMap.put(ID,attributes.get("gene"));
+        }
+        if (parent!=null && feature.getName().equalsIgnoreCase("exon")) {
+            idToChildrenMap.putIfAbsent(parent, new ArrayList<>());
+            idToChildrenMap.get(parent).add(feature);
+        }
 
         return feature;
     }
