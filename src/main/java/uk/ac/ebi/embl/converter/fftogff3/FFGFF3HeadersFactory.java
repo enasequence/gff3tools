8b3a874f396616b057f1f7bb7090122a2474855a
Ruben Paz <me@ruben.io>
2025-03-11 14:24:13 +0000

    Apply review suggestion

--- src/main/java/uk/ac/ebi/embl/converter/fftogff3/FFGFF3HeadersFactory.java
+++ src/main/java/uk/ac/ebi/embl/converter/fftogff3/FFGFF3HeadersFactory.java
@@ -5,23 +5,22 @@ import uk.ac.ebi.embl.api.entry.feature.Feature;
 import uk.ac.ebi.embl.converter.IConversionRule;
 import uk.ac.ebi.embl.converter.gff3.GFF3Headers;
 
+import java.util.Optional;
+
 public class FFGFF3HeadersFactory implements IConversionRule<Entry, GFF3Headers> {
 
     @Override
     public GFF3Headers from(Entry entry) {
 
-        for (Feature feature : entry.getFeatures().stream().sorted().toList()) {
+            Feature feature = Optional.ofNullable(entry.getPrimarySourceFeature())
+                    .orElseThrow(NoSourcePresent::new);
 
-            // Output header
-            if (feature.getName().equalsIgnoreCase("source")) {
-                String accession = entry.getPrimaryAccession() + ".1";
-                long start = feature.getLocations().getMinPosition();
-                long end = feature.getLocations().getMaxPosition();
-                return new GFF3Headers("3.1.26", accession, start, end);
-            }
-        }
-
-        throw new NoSourcePresent();
+            return new GFF3Headers(
+                    "3.1.26",
+                    entry.getPrimaryAccession() + ".1",
+                    feature.getLocations().getMinPosition(),
+                    feature.getLocations().getMaxPosition()
+            );
     }
 
     public static class NoSourcePresent extends ConversionError {
