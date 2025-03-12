31efc5b2bc428c72e197c4c4ac8aee729c72d7ef
Ruben Paz <me@ruben.io>
2025-03-12 09:46:41 +0000

    Check for null source feature when parsing sourc attributes

--- src/main/java/uk/ac/ebi/embl/converter/fftogff3/FFGFF3SourceAttributesFactory.java
+++ src/main/java/uk/ac/ebi/embl/converter/fftogff3/FFGFF3SourceAttributesFactory.java
@@ -6,13 +6,15 @@ import uk.ac.ebi.embl.api.entry.qualifier.Qualifier;
 import uk.ac.ebi.embl.converter.IConversionRule;
 import uk.ac.ebi.embl.converter.gff3.GFF3SourceMetadata;
 
+import java.util.Optional;
+
 public class FFGFF3SourceAttributesFactory implements IConversionRule<Entry, GFF3SourceMetadata> {
 
     @Override
     public GFF3SourceMetadata from(Entry entry) throws ConversionError {
 
-        Feature feature = entry.getPrimarySourceFeature();
-
+        Feature feature = Optional.ofNullable(entry.getPrimarySourceFeature())
+                .orElseThrow(FFGFF3HeadersFactory.NoSourcePresent::new);
         String organism = feature.getQualifiers("organism").stream().findFirst()
                 .map(Qualifier::getValue)
                 .orElseGet(() -> null);
