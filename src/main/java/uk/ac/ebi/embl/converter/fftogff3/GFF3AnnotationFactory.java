aeefc6734dc81332a38d6ac24bf23af319cd6eb8
Ruben Paz <me@ruben.io>
2025-04-01 09:09:04 +0000

    Circular structures support (#6)
    * Tests for circular structures
    
    * Extract circular logic methods
    
    * Rename to accession
    
    * Remove unnecesary circular checks

--- src/main/java/uk/ac/ebi/embl/converter/fftogff3/GFF3AnnotationFactory.java
+++ src/main/java/uk/ac/ebi/embl/converter/fftogff3/GFF3AnnotationFactory.java
@@ -14,22 +14,39 @@ import java.util.*;
 import java.util.stream.Collectors;
 import uk.ac.ebi.embl.api.entry.Entry;
 import uk.ac.ebi.embl.api.entry.feature.Feature;
+import uk.ac.ebi.embl.api.entry.location.CompoundLocation;
+import uk.ac.ebi.embl.api.entry.location.Location;
 import uk.ac.ebi.embl.api.entry.qualifier.Qualifier;
+import uk.ac.ebi.embl.api.entry.sequence.Sequence;
 import uk.ac.ebi.embl.converter.IConversionRule;
 import uk.ac.ebi.embl.converter.gff3.*;
 import uk.ac.ebi.embl.converter.utils.ConversionEntry;
 import uk.ac.ebi.embl.converter.utils.ConversionUtils;
 
 public class GFF3AnnotationFactory implements IConversionRule<Entry, GFF3Annotation> {
+  /// This property is used to keep track of the presence of circular landmarks on the annotation.
+  /// If the topology is circular and no circular landmark was found, then a new "region"
+  /// is added using the "createLandmarkFeature" method.
+  boolean missingCircularLandmark;
+
+  ///  Keeps track of all the features belonging to a gene.
   Map<String, List<GFF3Feature>> geneMap;
+  ///  List of features that do not belong to a gene.
   List<GFF3Feature> nonGeneFeatures;
 
   @Override
   public GFF3Annotation from(Entry entry) {
+
     geneMap = new LinkedHashMap<>();
     nonGeneFeatures = new ArrayList<>();
-    entry.setPrimaryAccession(entry.getPrimaryAccession() + ".1");
-    entry.getSequence().setAccession(entry.getSequence().getAccession() + ".1");
+    missingCircularLandmark = entry.getSequence().getTopology() == Sequence.Topology.CIRCULAR;
+
+    String accession = entry.getSequence().getAccession();
+
+    // TODO: We need to handle accession versions
+    entry.setPrimaryAccession(accession + ".1");
+    entry.getSequence().setAccession(accession + ".1");
+
     GFF3Directives directives = new GFF3DirectivesFactory().from(entry);
     try {
       Map<String, List<ConversionEntry>> featureMap = ConversionUtils.getFF2GFF3FeatureMap();
@@ -49,14 +66,45 @@ public class GFF3AnnotationFactory implements IConversionRule<Entry, GFF3Annotat
         // Rule: Throw an error if we find an unmapped feature
         if (first.isEmpty()) throw new Exception("Mapping not found for " + feature.getName());
 
+        updateCircularLandmarkPresence(feature);
+
         buildGeneFeatureMap(entry.getPrimaryAccession(), feature);
+      }
+
+      // For circular topologies; We have not found a circular feature so we must include a region
+      // encompasing all source.
+      if (missingCircularLandmark) {
+        nonGeneFeatures.add(createLandmarkFeature(accession, entry));
       }
       sortFeaturesAndAssignId();
 
       return new GFF3Annotation(directives, geneMap, nonGeneFeatures);
     } catch (Exception e) {
-      throw new ConversionError();
+      throw new RuntimeException(e);
     }
+  }
+
+  private void updateCircularLandmarkPresence(Feature feature) {
+    if (missingCircularLandmark) {
+      // A feature that has "circular_RNA" can be used as a landmark for circular topologies.
+      if (!feature.getQualifiers("circular_RNA").isEmpty()) {
+        missingCircularLandmark = false;
+      }
+    }
+  }
+
+  private GFF3Feature createLandmarkFeature(String name, Entry entry) {
+    CompoundLocation<Location> locations = entry.getPrimarySourceFeature().getLocations();
+    return new GFF3Feature(
+        entry.getPrimaryAccession(),
+        ".",
+        "region",
+        locations.getMinPosition(),
+        locations.getMaxPosition(),
+        ".",
+        "+",
+        ".",
+        Map.of("ID", name, "Is_circular", "true"));
   }
 
   private GFF3Feature transformFeature(String accession, Feature ffFeature, Optional<String> gene) {
