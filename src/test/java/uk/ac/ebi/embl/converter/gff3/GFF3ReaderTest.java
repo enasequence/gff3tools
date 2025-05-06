7e9cc47dcb781a9d34916633130c14fa74f1be99
Rajkumar-D <rajkumar.devraj@gmail.com>
2025-05-06 13:25:45 +0100

    Merge branch to handle joins

--- src/test/java/uk/ac/ebi/embl/converter/gff3/GFF3ReaderTest.java
+++ src/test/java/uk/ac/ebi/embl/converter/gff3/GFF3ReaderTest.java
@@ -10,6 +10,7 @@
  */
 package uk.ac.ebi.embl.converter.gff3;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.*;
