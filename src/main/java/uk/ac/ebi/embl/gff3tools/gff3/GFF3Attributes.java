165115638694075ad79ac44027a0a95d8274d89f
Iva Tutis <tutis@ebi.ac.uk>
2025-10-28 11:08:26 +0000

    wip

--- src/main/java/uk/ac/ebi/embl/gff3tools/gff3/GFF3Attributes.java
+++ src/main/java/uk/ac/ebi/embl/gff3tools/gff3/GFF3Attributes.java
@@ -11,9 +11,49 @@
 package uk.ac.ebi.embl.gff3tools.gff3;
 
 public class GFF3Attributes {
-    public static String PROTEIN_ID = "protein_id";
-    public static String ATTRIBUTE_ID = "ID";
-    public static String LOCUS_TAG = "locus_tag";
-    public static String GENE = "gene";
-    public static String GENE_SYNONYM = "gene_synonym";
+
+
+    public static final String ORGANELLE = "organelle";
+    public static final String SEGMENT = "segment";
+    public static final String CHROMOSOME = "chromosome";
+    public static final String CLONE = "clone";
+    public static final String OLD_LOCUS_TAG = "old_locus_tag";
+    public static final String SUB_STRAIN = "sub_strain";
+    public static final String SUB_CLONE = "sub_clone";
+    public static final String ISOLATION_SOURCE = "isolation_source";
+    public static final String LOCUS_TAG = "locus_tag";
+    public static final String PROTEIN_ID = "protein_id";
+    public static final String ATTRIBUTE_ID = "ID";
+    public static final String MAP = "map";
+    public static final String PCR_PRIMERS = "PCR_primers";
+    public static final String SATELLITE = "satellite";
+    public static final String PROVIRAL = "proviral";
+    public static final String VIRION = "virion";
+    public static final String REARRANGED = "rearranged";
+    public static final String GERM_LINE = "germline";
+    public static final String ENVIRONMENTAL_SAMPLE = "environmental_sample";
+    public static final String STRAIN = "strain";
+    public static final String CULTIVAR = "cultivar";
+    public static final String VARIETY = "variety";
+    public static final String CULTURE_COLLECTION = "culture_collection";
+    public static final String SPECIMEN_VOUCHER = "specimen_voucher";
+    public static final String PSEUDO = "pseudo";
+    public static final String PSEUDOGENE = "pseudogene";
+    public static final String PRODUCT = "product";
+    public static final String NOTE = "note";
+    public static final String CLONE_LIB = "clone_lib";
+    public static final String ORGANISM = "organism";
+    public static final String MACRO_NUCLEAR = "macronuclear";
+    public static final String GENE = "gene";
+    public static final String MOL_TYPE = "mol_type";
+    public static final String CIRCULAR_RNA = "Is_circular";
+    public static final String PROCESSED = "processed";
+    public static final String UNPROCESSED = "unprocessed";
+    public static final String UNITARY = "unitary";
+    public static final String ALLELIC = "allelic";
+    public static final String UNKNOWN = "unknown";
+    public static final String ARTIFICIAL_LOCATION = "artificial_location";
+    public static final String RIBOSOMAL_SLIPPAGE = "ribosomal_slippage";
+    public static final String TRANS_SPLICING = "trans_splicing";
+    public static final String GENE_SYNONYM = "gene_synonym";
 }
