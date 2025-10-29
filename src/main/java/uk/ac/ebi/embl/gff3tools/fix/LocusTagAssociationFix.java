c239f5794c594b40657fe38ae1ccae1519c159e1
Iva Tutis <tutis@ebi.ac.uk>
2025-10-29 10:03:30 +0000

    wip

---
+++ src/main/java/uk/ac/ebi/embl/gff3tools/fix/LocusTagAssociationFix.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.embl.gff3tools.fix;
+
+import uk.ac.ebi.embl.gff3tools.gff3.GFF3Attributes;
+import uk.ac.ebi.embl.gff3tools.gff3.GFF3Feature;
+
+public class LocusTagAssociationFix {
+
+    public final String LOCUS_TAG = GFF3Attributes.LOCUS_TAG;
+
+
+    public LocusTagAssociationFix(){
+
+    }
+
+    public void fix(GFF3Feature feature, int line) {
+
+    }
+}
---
+++ src/test/java/uk/ac/ebi/embl/gff3tools/fix/LocusTagAssociationFixTest.java
@@ -0,0 +1,4 @@
+package uk.ac.ebi.embl.gff3tools.fix;
+
+public class LocusTagAssociationFixTest {
+}
