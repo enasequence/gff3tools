a96cd63460816209d628fd6a0727745e26e6b5be
Iva Tutis <tutis@ebi.ac.uk>
2026-01-12 13:35:43 +0000

    unneccessary-logic-fixes

--- src/main/java/uk/ac/ebi/embl/gff3tools/fasta/sequenceutils/SequenceAlphabet.java
+++ src/main/java/uk/ac/ebi/embl/gff3tools/fasta/sequenceutils/SequenceAlphabet.java
@@ -49,15 +49,8 @@ public final class SequenceAlphabet {
         for (int i = 0; i < allowed.length; i++) {
             if (allowed[i]) {
                 char c = (char) i;
-
-                // Render unprintables safely
                 String display;
-                if (c >= 32 && c < 127) {
-                    display = Character.toString(c);
-                } else {
-                    display = String.format("\\x%02X", i); // e.g. non-printable → \x1B
-                }
-
+                display = Character.toString(c);
                 if (!first) sb.append(", ");
                 sb.append(display);
                 first = false;
