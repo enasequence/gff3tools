354a40c53abcdf61d8b42ca472a7a6e4aae371dc
Iva Tutis <tutis@ebi.ac.uk>
2026-01-19 22:43:28 +0000

    added-parser

---
+++ src/main/java/uk/ac/ebi/embl/gff3tools/fasta/headerutils/FastaHeader.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 EMBL - European Bioinformatics Institute
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package uk.ac.ebi.embl.gff3tools.fasta.headerutils;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FastaHeader {
+    @JsonProperty("description")
+    String description; // mandatory
+
+    @JsonProperty("molecule_type")
+    @JsonAlias({"molecule-type", "molecule type", "moleculetype"})
+    String moleculeType; // mandatory
+
+    @JsonProperty("topology")
+    String topology; // mandatory
+
+    @JsonProperty("chromosome_type")
+    @JsonAlias({"chromosome-type", "chromosome type", "chromosometype"})
+    String chromosomeType; // optional (doesn't have to be in the json at all)
+
+    @JsonProperty("chromosome_location")
+    @JsonAlias({"chromosome-location", "chromosome location", "chromosomelocation"})
+    String chromosomeLocation; // optional
+
+    @JsonProperty("chromosome_name")
+    @JsonAlias({"chromosome-name", "chromosome name", "chromosomename"})
+    String chromosomeName; // optional
+}
---
+++ src/main/java/uk/ac/ebi/embl/gff3tools/fasta/headerutils/JsonHeaderParser.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2025 EMBL - European Bioinformatics Institute
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package uk.ac.ebi.embl.gff3tools.fasta.headerutils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.util.*;
+import uk.ac.ebi.embl.gff3tools.exception.FastaHeaderParserException;
+
+public class JsonHeaderParser {
+    private static final ObjectMapper MAPPER = JsonMapper.builder()
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
+            .build();
+    ;
+
+    public ParsedHeader parse(String headerLine) throws FastaHeaderParserException {
+
+        // char limit according to the spec
+        if (headerLine.length() > 4096) {
+            throw new FastaHeaderParserException(
+                    "FASTA header should contain a maximum of 4096 characters according to the specification.");
+        }
+
+        String rest = headerLine.substring(1);
+        int pipe = rest.indexOf('|');
+
+        // parse id
+        String idPart = (pipe >= 0 ? rest.substring(0, pipe) : rest).trim();
+        String id = idPart.isEmpty() ? "" : idPart.split("\\s+")[0];
+        if (Objects.equals(id, "")) {
+            throw new FastaHeaderParserException("FASTA header should contain the id, but no id was provided.");
+        }
+
+        // parse header
+        FastaHeader header = new FastaHeader();
+        if (pipe >= 0) {
+            header = parseHeaderJson(rest.substring(pipe + 1).trim());
+        }
+
+        return new ParsedHeader(id, header);
+    }
+
+    private static FastaHeader parseHeaderJson(String raw) throws FastaHeaderParserException {
+        if (raw == null || raw.isBlank()) {
+            throw new FastaHeaderParserException("FASTA header contains a '|', but no JSON object was provided.");
+        }
+
+        String normalised = normaliseRawJsonString(raw);
+
+        try {
+            FastaHeader header = MAPPER.readValue(normalised, FastaHeader.class);
+            return header;
+
+        } catch (JsonProcessingException e) {
+            throw new FastaHeaderParserException(
+                    "Malformed FASTA header JSON: " + normalised + " due to " + e.getMessage(), e);
+        }
+    }
+
+    private static String normaliseRawJsonString(String raw) {
+        return raw.replace('\u201C', '"')
+                .replace('\u201D', '"')
+                .replace('\u2018', '\'')
+                .replace('\u2019', '\'')
+                .replace('\u00A0', ' ')
+                .trim();
+    }
+}
