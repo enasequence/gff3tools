35aa5217860b02f597e590b543a614f68cd2bfd4
Iva Tutis <tutis@ebi.ac.uk>
2026-08-11 12:03:33 +0100

    corrected

--- src/main/java/uk/ac/ebi/embl/gff3tools/validation/builtin/FeatureLocationValidation.java
+++ src/main/java/uk/ac/ebi/embl/gff3tools/validation/builtin/FeatureLocationValidation.java
@@ -12,8 +12,13 @@ package uk.ac.ebi.embl.gff3tools.validation.builtin;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import uk.ac.ebi.embl.gff3tools.exception.ValidationException;
 import uk.ac.ebi.embl.gff3tools.gff3.GFF3Feature;
+import uk.ac.ebi.embl.gff3tools.sequence.fasta.header.FastaHeaderProvider;
+import uk.ac.ebi.embl.gff3tools.sequence.fasta.header.utils.ControlledVocabularyUtils;
+import uk.ac.ebi.embl.gff3tools.sequence.fasta.header.utils.FastaHeader;
 import uk.ac.ebi.embl.gff3tools.utils.ValidationUtils;
 import uk.ac.ebi.embl.gff3tools.validation.ValidationContext;
 import uk.ac.ebi.embl.gff3tools.validation.meta.ExitMethod;
@@ -24,6 +29,7 @@ import uk.ac.ebi.embl.gff3tools.validation.meta.ValidationMethod;
 import uk.ac.ebi.embl.gff3tools.validation.meta.ValidationPriority;
 import uk.ac.ebi.embl.gff3tools.validation.meta.ValidationType;
 
+@Slf4j
 @Gff3Validation(
         name = "FEATURE_LOCATION",
         description = "Validates that feature and sequence-region coordinates are within the sequence bounds")
@@ -32,6 +38,7 @@ public class FeatureLocationValidation implements Validation {
     private static final String RULE_FEATURE_END_EXCEEDS_SEQUENCE_LENGTH = "FEATURE_END_EXCEEDS_SEQUENCE_LENGTH";
     private static final String FEATURE_END_EXCEEDS_SEQUENCE_LENGTH =
             "The end position of the location \"%s\" is greater than the length of the sequence (\"%d\").";
+    private static final String FEATURE_END_BELOW_ONE = "The end position of the location \"%s\" is less than 1.";
 
     private static final String RULE_FEATURE_START_BELOW_ONE = "FEATURE_START_BELOW_ONE";
     private static final String FEATURE_START_BELOW_ONE = "The start position of the location \"%s\" is less than 1.";
@@ -47,11 +54,37 @@ public class FeatureLocationValidation implements Validation {
             type = ValidationType.FEATURE,
             priority = ValidationPriority.LOW)
     public void validateFeatureEndWithinSequence(GFF3Feature feature, int line) throws ValidationException {
+        if (feature.getEnd() < 1) {
+            throw new ValidationException(
+                    RULE_FEATURE_END_EXCEEDS_SEQUENCE_LENGTH, line, FEATURE_END_BELOW_ONE.formatted(feature.getEnd()));
+        }
+
+        // If no FASTA header source registered for this run -> nothing to validate as we do not know the topology.
+        // otherwise get topology from fastaHeader
+        if (!context.contains(FastaHeaderProvider.class)
+                || context.get(FastaHeaderProvider.class)
+                        .getHeader(feature.accession())
+                        .isEmpty()) {
+            log.warn("No FASTA header found for accession {}", feature.accession());
+            return;
+        }
+        FastaHeader header = context.get(FastaHeaderProvider.class)
+                .getHeader(feature.accession())
+                .get();
+        Optional<ControlledVocabularyUtils.Topology> topologyOpt =
+                ControlledVocabularyUtils.Topology.fromValue(header.getTopology());
+        if (topologyOpt.isEmpty()) {
+            /***this field is mandatory, if not present the issue will be caught by {@link FastaHeaderFormatValidation}***/
+            return;
+        }
+        ControlledVocabularyUtils.Topology topology = topologyOpt.get();
+
         Long lastBaseIndex = ValidationUtils.resolveSequenceLength(feature.accession(), sequenceLengthCache, context);
         if (lastBaseIndex == null) {
             return;
         }
-        if (feature.getEnd() > lastBaseIndex) {
+
+        if (topology.equals(ControlledVocabularyUtils.Topology.LINEAR) && feature.getEnd() > lastBaseIndex) {
             String location = feature.getStart() + ".." + feature.getEnd();
             throw new ValidationException(
                     RULE_FEATURE_END_EXCEEDS_SEQUENCE_LENGTH,
