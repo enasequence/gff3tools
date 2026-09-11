430345f4b5b3b89e3fc998ebf8bcfa9bca119ec4
rajkumar <rajkumar.devraj@gmail.com>
2026-09-11 12:55:12 +0100

    spotless

--- src/test/java/uk/ac/ebi/embl/gff3tools/cli/ValidationCommandTest.java
+++ src/test/java/uk/ac/ebi/embl/gff3tools/cli/ValidationCommandTest.java
@@ -26,7 +26,6 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.GZIPOutputStream;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
